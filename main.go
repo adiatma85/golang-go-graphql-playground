4424a1eeea11fbcab63c1e86438ce3d82b38a6a5
ramdani koernia <adiatma85@gmail.com>
2024-08-01 21:41:34 +0700

    basic skeletal with graphql

--- main.go
+++ main.go
@@ -23,29 +23,6 @@ const (
 )
 
 func main() {
-	// KODINGAN YANG LAMA
-
-	// // Using chi router
-	// router := chi.NewRouter()
-
-	// // Kalau kaya gini berarti semua router kenak dong??
-	// // Jawaban iya, dia kenak semuanya
-	// router.Use(auth.Middleware())
-
-	// mysql.InitDB()
-	// defer mysql.CloseDB()
-	// server := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
-	// router.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	// router.Handle("/query", server)
-
-	// log.Printf("connect to http://localhost:%s/ for GraphQL playground", cfg.Gin.Port)
-	// log.Fatal(http.ListenAndServe(":"+cfg.Gin.Port, router))
-
-	// KODINGAN YANG LAMA
-
-	// Build the config
-	// Assume the config is exist in the first place
-
 	// Read the Config first
 	cfg := config.Init()
 	configreader := configreader.Init(configreader.Options{
